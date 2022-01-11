aeace82bf123e447cfe81d9a2d72e648e8e2af0c
never88gone <husanbo@126.com>
2022-01-11 17:11:48 +0800

    先可以做基本的展示，页面各种需要调试

--- swift/interview-assignments/interview-assignments/ContentView.swift
+++ swift/interview-assignments/interview-assignments/ContentView.swift
@@ -14,26 +14,43 @@ struct ContentView: View {
     @State private var selection = 0
     @State private var todoStr = ""
     @State private var descStr = "aaa"
-
+    
     @EnvironmentObject private var todoManager: TodoManager
     
     func deleteRow(at offsets:IndexSet) {
         todoManager.todos.remove(atOffsets: offsets)
     }
-
+    
     var body: some View {
-        NavigationView{
+        let todoManager = TodoManager()
+        todoManager.addTask(info: "desc1",title: "haha" )
+        todoManager.addTask(info: "desc2",title: "haha" )
+        todoManager.addTask(info: "desc2",title: "haha1" )
+        todoManager.addTask(info: "desc2")
+        todoManager.addTask(info: "desc2")
+        todoManager.addTask(info: "desc2")
+        todoManager.curTodo=todoManager.todos[0]
+        let keys = [String](todoManager.getShowTodos().keys)
+        return  NavigationView{
             VStack {
+                List {
+                    ForEach(keys, id: \.self) { oneKey in
+                        Section(header: Text(oneKey).font(.title))
+                        {
+                            let todolist = [Todo](todoManager.getShowTodos()[oneKey] ?? [Todo]())
+                            ForEach(todolist) { oneTodo in
+                                ToDoTVCell(todo:oneTodo)
+                            }.onDelete(perform: deleteRow)
+                        }
+                    }
 
-                List(todoManager.todos) { todo in ToDoTVCell(todo: todo)
-                    
-                }.onDelete(perform: removeRows)
-                
+                }.listStyle(GroupedListStyle())
+
                 BottomInputView(todo: todoManager.curTodo)
-                
+
             }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0,  maxHeight: .infinity, alignment: .center)
-            .navigationTitle(Text("List").font(.largeTitle))
-            .background(Color(.init(red: 0xf7/255, green: 0xf7/255, blue: 0xf7/255, alpha: 0)))
+                .navigationTitle(Text("List").font(.largeTitle))
+                .background(Color(.init(red: 0xf7/255, green: 0xf7/255, blue: 0xf7/255, alpha: 0)))
         }
     }
 }
@@ -41,9 +58,9 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let todoManager = TodoManager()
-        todoManager.addTask(info: "desc1")
-        todoManager.addTask(info: "desc2")
-        todoManager.addTask(info: "desc2")
+        todoManager.addTask(info: "desc1",title: "haha" )
+        todoManager.addTask(info: "desc2",title: "haha" )
+        todoManager.addTask(info: "desc2",title: "haha1" )
         todoManager.addTask(info: "desc2")
         todoManager.addTask(info: "desc2")
         todoManager.addTask(info: "desc2")
--- swift/interview-assignments/interview-assignments/TodoManager.swift
+++ swift/interview-assignments/interview-assignments/TodoManager.swift
@@ -11,15 +11,26 @@ import Combine
 class TodoManager: NSObject, ObservableObject {
     @Published  var curTodo: Todo=Todo(info: "")
     @Published var todos = [Todo]()
+
     
+    /**数据展示的时候使用**/
+    func getShowTodos() -> Dictionary<String, [Todo]>{
+        return Dictionary(grouping: todos, by: {
+            $0.title
+        })
+    }
     func addTask(info: String) {
         todos.append(Todo(info: info))
+    }
+    func addTask(info: String ,title: String) {
+        todos.append(Todo(info: info,title: title))
     }
 }
 
 struct Todo: Identifiable {
     var id:  UUID = UUID()
     var info: String
+    var title: String = ""
     var hasDelete : Bool=false
 }
 
